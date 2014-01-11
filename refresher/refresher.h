1217295455be1d0c9e6cdc73fb082d50b0eca6c3
armink <armink.ztl@gmail.com>
2014-01-11 14:46:45 +0800

    1、【优化】刷新器的数据结构
    Signed-off-by: armink <armink.ztl@gmail.com>

--- refresher/refresher.h
+++ refresher/refresher.h
@@ -30,11 +30,21 @@ typedef enum{
 	REFRESHER_NO_ERR,                 /**< no error */
 }RefresherErrCode;
 
+/* the job wait mode in refresher */
+typedef enum{
+	JOB_TYPE_BLOCK_RESOURCE,          /**< the job will block by resource */
+	JOB_TYPE_BLOCK_LOGIC_WAIT,        /**< the job will block by some logic wait */
+	JOB_TYPE_NONBLOCK,                /**< the job will not block,so there is no OS delay in job */
+}JobWaitMode;
+
+
 /* RefreshJob is an auto refresh job for a Cache data. */
 typedef struct _RefreshJob{
 	char  name[CACHE_NAME_MAX];         /**< the name of the CacheData is refreshed.@see CacheData */
 	int8_t level;                       /**< refresh level.If level is -1,RefreshJob will stop work */
 	uint8_t period;                     /**< refresh time = period * refresher tick. @see Refresher */
+	uint8_t times;                      /**< job running times.If equal 0,the job will continuous running */
+	JobWaitMode waitMode;               /**< job wait mode @see JobWaitMode */
 	void* (*refreshProcess)(void *arg); /**< it will call when the RefreshJob need wrok */
 	struct _RefreshJob* next;           /**< point to next CacheData */
 } RefreshJob , *pRefreshJob;
@@ -44,7 +54,8 @@ typedef struct _Refresher {
 	RefresherErrCode (*start)(struct _Refresher* const refresher);
 	RefresherErrCode (*stop)(struct _Refresher* const refresher);
 	RefresherErrCode (*add)(struct _Refresher* const refresher, char* name,
-			int8_t level, uint8_t period, void* (*refreshProcess)(void *arg));
+			int8_t level, uint8_t period, uint8_t times, JobWaitMode waitMode,
+			void* (*refreshProcess)(void *arg));
 	RefresherErrCode (*del)(struct _Refresher* const refresher,
 			const char* name);
 	RefresherErrCode (*setLevel)(struct _Refresher* const refresher, char* name,
@@ -54,10 +65,12 @@ typedef struct _Refresher {
 	RefresherErrCode (*setProcess)(struct _Refresher* const refresher,
 			char* name, void* (*refreshProcess)(void *arg));
 	RefresherErrCode (*control)(struct _Refresher* const refresher, char* name,
-			int8_t level, uint8_t period, void* (*refreshProcess)(void *arg));
-	uint32_t tickTime;                  /**< the Refresher work tick time. unit:Millisecond */
+			int8_t level, uint8_t period, uint8_t times, JobWaitMode waitMode,
+			void* (*refreshProcess)(void *arg));
 	char name[CACHE_NAME_MAX];          /**< the name of Refresher */
-	pRefreshJob queueHead;              /**< the refresh job queue */
+	uint32_t tickTime;                  /**< the Refresher work tick time. unit:Millisecond */
+	pRefreshJob jobWaitHead;            /**< the refresh job queue */
+	pRefreshJob jobReadyHead;           /**< the refresh job queue */
 } Refresher, *pRefresher;
 
 
